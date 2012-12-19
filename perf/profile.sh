776172a7b5b20fb6e718b02eb8d7c561879ab427
Carsten Block <mail@carstenblock.org>
2012-12-20 00:16:19 +0100

    Automatically generated pdf profiling results

--- perf/profile.sh
+++ perf/profile.sh
@@ -1,3 +1,10 @@
 rm -rf perf/results
 mkdir perf/results
+
 CPUPROFILE_REALTIME=1 CPUPROFILE_FREQUENCY=1000 bundle exec ruby perf/profile.rb
+
+for f in perf/results/*.profile
+do
+  echo "Creating pdf for $f..."
+  pprof.rb --pdf $f > "$f.pdf"
+done
